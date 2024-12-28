ec3958feea1cae37bfd847439491aaef4f8d4390
Harshit Budhraja <harshitbudhraja1301@gmail.com>
2024-12-28 14:04:12 +0530

    feat: initial boilerplate

---
+++ src/content/index.ts
@@ -0,0 +1,5 @@
+async function initContentScript() {
+  console.log("Hello, world from content script!");
+}
+
+initContentScript();
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    worker: './src/worker/index.ts',
+    content: './src/content/index.ts'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  mode: 'development',
+  devtool: 'cheap-module-source-map',
+};
